f6afb7e0072c037a67d822e7f2135591a1b39860
elikese <elikese@gmail.com>
2025-11-03 20:25:33 +0900

    new: wrapper

--- src/main/java/_29_Wrapper/Main.java
+++ src/main/java/_29_Wrapper/Main.java
@@ -12,6 +12,7 @@ public class Main {
         -> null로 명시하면 좋을텐데..
         */
 
+        // 원시자료형 -> 래퍼클래스(박싱)
         // int를 감싸는 래핑클래스: Integer
         Integer integer1 = Integer.valueOf(90); // 기본
         Integer integer2 = 90; // 자동으로 컴파일러가 16번라인 코드로 변환
@@ -19,9 +20,30 @@ public class Main {
         // Long을 감싸는 래핑클래스: Long
         Long long1 = Long.valueOf(100L);
         Long long2 = 100L;
-
+        
+        // jdk 설치하면 같이 설치되는 java.lang 패키지에 정의된걸 사용하면 된다
         // double -> Double 클래스
         // boolean -> Boolean 클래스
+        // float -> Float 클래스
+        
+        // 객체 -> 메서드사용, Object를 상속받았음
+        // 원시자료형이 아니니까 == 연산자 x
+        Integer a = Integer.valueOf(1000);
+        Integer b = Integer.valueOf(1000);
+
+        System.out.println(a == b); // false
+        System.out.println(a.equals(b)); // 올바른 방법. true
+
+        // 반대로 래퍼클래스 -> 원시자료형(언박싱)
+        // 객체.intValue(), 객체.longValue(), 객체.doubleValue()...
+        int int1 = a.intValue();
+
+        // 미리 정의되어있는 유틸메서드
+        int max = Integer.max(10, 20); // 박싱 -> 언박싱
+        int min = Integer.min(10, 20); // 박싱 -> 언박싱
+        int sum = Integer.sum(10, 20); // 박싱 -> 언박싱
+        
+
 
 
     }
---
+++ src/main/java/_29_Wrapper/Practice.java
@@ -0,0 +1,13 @@
+package _29_Wrapper;
+
+public class Practice {
+    public static void main(String[] args) {
+        int[] scores = {85, 92, 78, 96, 88};
+        // 1. Integer 배열로 변환해주세요
+        Integer[] integerScores = new Integer[scores.length];
+        // 2. 최고점, 최저점을 적절한 Integer 메서드를 사용해서 구해주세요
+        // max():두개 비교해서 큰 값 리턴, min():두개 비교해서 작은 값 리턴
+        Integer maxScore = 0;
+        Integer minScore = 0;
+    }
+}
